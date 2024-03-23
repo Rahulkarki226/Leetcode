eb99648393e200fe555ef5e04d95079f0ebbbbf8
Rahul karki <118829063+Rahulkarki226@users.noreply.github.com>
2024-03-24 00:03:08 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (58.18%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+         while(tx>sx && ty>sy)
+         {
+            if(tx>ty)
+               tx%=ty;
+            else
+               ty%=tx;
+         }
+         if(sx==tx && ty>=sy)
+            return (ty-sy)%tx==0;
+         if(sy==ty && tx>=sx)
+            return (tx-sx)%ty==0;
+
+        return false;
+    }
+};
